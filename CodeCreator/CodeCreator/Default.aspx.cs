89aacc59f20f8a3b4732f6607bd82a33f1aafe42
BelMoBrand <mobrandsoul@gmail.com>
2016-11-24 16:53:04 +0800

    二维码的制作

---
+++ CodeCreator/CodeCreator/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing.Imaging;
+using MessagingToolkit.QRCode.Codec;
+
+namespace CodeCreator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var qRCode = new QRCodeEncoder();
+            var img = qRCode.Encode("zhang");
+            img.Save("E:\\Code\\GitHub\\QRCode\\CodeCreator\\CodeCreator\\Contents\\Images\\img.jpg", ImageFormat.Jpeg);
+            QRCode.ImageUrl = "~/Contents/Images/img.jpg";
+        }
+    }
+}
